5c44343301fbc7936c3fb2ee4092022f69a7c64b
jitunmohajan <jitunmohajan@gmail.com>
2023-06-11 12:40:47 +0600

    update some code

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import express, { Application } from 'express'
-import cors from 'cors'
-import { UserRoutes } from './app/modulers/users/user.route'
-import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import express, { Application } from 'express';
+import cors from 'cors';
+import { UserRoutes } from './app/modulers/user/user.route';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 // import ApiError from './errors/ApiError'
-const app: Application = express()
+const app: Application = express();
 // const port = 3000
 
-app.use(cors())
+app.use(cors());
 
 // parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Application routes
-app.use('/api/v1/users/', UserRoutes)
+app.use('/api/v1/users/', UserRoutes);
 
 // testing server
 // app.get('/', async(req: Request, res: Response, next:NextFunction) => {
@@ -21,6 +21,6 @@ app.use('/api/v1/users/', UserRoutes)
 // })
 
 // globall error
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
-export default app
+export default app;
