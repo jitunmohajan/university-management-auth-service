fd7fa66eddca1735dbd4cc9dcb4d2c8249bf5c88
jitunmohajan <jitunmohajan@gmail.com>
2023-06-15 13:15:20 +0600

    update some changes

--- src/app/modulers/academicSemester/academicSemester.controller.ts
+++ src/app/modulers/academicSemester/academicSemester.controller.ts
@@ -12,14 +12,14 @@ const createSemester = catchAsync(
       academicSemesterData
     );
 
-    next();
-
     sendResponse<IAcademicSemester>(res, {
       statusCode: httpStatus.OK,
       success: true,
       message: 'Aademic semester created successfully!',
       data: result,
     });
+
+    next();
   }
 );
 
