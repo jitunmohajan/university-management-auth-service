42415555f69249f2354f759e6669aa0535c917ee
jitunmohajan <jitunmohajan@gmail.com>
2023-06-17 14:13:02 +0600

    update changes

--- src/app/modulers/academicSemester/academicSemester.service.ts
+++ src/app/modulers/academicSemester/academicSemester.service.ts
@@ -1,10 +1,15 @@
 import httpStatus from 'http-status';
 import ApiError from '../../../errors/ApiError';
 import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
-import { IAcademicSemester } from './academicSemester.inteface';
+import {
+  IAcademicSemester,
+  IAcademicSemesterFilters,
+} from './academicSemester.inteface';
 import { AcademicSemester } from './academicSemester.model';
 import { IPaginationOptions } from '../../../interfaces/pagination';
 import { IGenericResponse } from '../../../interfaces/common';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { SortOrder } from 'mongoose';
 
 const createSemester = async (
   payload: IAcademicSemester
@@ -18,11 +23,62 @@ const createSemester = async (
 };
 
 const getAllSemesters = async (
+  filters: IAcademicSemesterFilters,
   paginationOptions: IPaginationOptions
 ): Promise<IGenericResponse<IAcademicSemester[]>> => {
-  const { page = 1, limit = 10 } = paginationOptions;
-  const skip = (page - 1) * limit;
-  const result = await AcademicSemester.find().sort().skip(skip).limit(limit);
+  const { searchTerm } = filters;
+
+  const academicSemesterSearchableFilters = ['titles', 'code', 'year'];
+  const andConditions = [];
+  if (searchTerm) {
+    andConditions.push({
+      $or: academicSemesterSearchableFilters.map(field => ({
+        [field]: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      })),
+    });
+  }
+
+  // const andConditions =[
+  //   {
+  //     $or:[
+  //       {
+  //         title:{
+  //           $regex: searchTerm,
+  //           $options:'i'
+  //         }
+  //       },
+  //       {
+  //         code:{
+  //           $regex: searchTerm,
+  //           $options:'i'
+  //         }
+  //       },
+  //       {
+  //         year:{
+  //           $regex: searchTerm,
+  //           $options:'i'
+  //         }
+  //       }
+  //     ]
+  //   }
+  // ];
+
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelpers.calculatePagination(paginationOptions);
+
+  const sortCondition: { [key: string]: SortOrder } = {};
+
+  if (sortBy && sortOrder) {
+    sortCondition[sortBy] = sortOrder;
+  }
+
+  const result = await AcademicSemester.find({ $and: andConditions })
+    .sort(sortCondition)
+    .skip(skip)
+    .limit(limit);
   const total = await AcademicSemester.countDocuments();
   return {
     meta: {
